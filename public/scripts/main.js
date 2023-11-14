a4046356f5ab4202477cc112f34054996f87427b
Ho Nyeong <dev.honing@gmail.com>
2023-11-14 18:16:07 +0900

    Feat: addTask() 함수에 체크박스 생성 로직 추가

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,8 @@
 // main.js
 
+// 빈 배열 생성
+const tasks = [];
+
 // 일정 목록 추가하는 함수 생성
 function addTask() {
   const taskInput = document.getElementById("taskInput");
@@ -7,20 +10,35 @@ function addTask() {
   const ul = document.getElementById("taskList");
 
   if ( taskText !== ''){
-    // span 생성
-    const taskSpan = document.createElement('span');
-    
-    // span에 taskText 내용 추가
-    taskSpan.textContent = taskText;
-
     // li 생성
     const taskLi = document.createElement('li');
 
-    // li에 span을 자식 요소로 추가
+    // span 생성
+    const taskSpan = document.createElement('span');
+    
+    // 체크박스 생성
+    const checkbox = document.createElement("input");
+    checkbox.type = 'checkbox';
+    checkbox.checked = false;
+
+    // 체크박스의 변경 이벤트에 대한 핸들러를 등록
+    checkbox.onchange = function () {
+      completeTask(taskText);
+    }
+
+    // span에 taskText 내용 추가
+    taskSpan.textContent = taskText;
+
+    // li에 체크박스, span을 자식 요소로 추가
+    taskLi.appendChild(checkbox);
     taskLi.appendChild(taskSpan);
 
     // ul에 li를 자식 요소로 추가
     ul.appendChild(taskLi);
+
+    // task 객체 생성 및 tasks 배열에 추가
+    const task = { text: taskText, isCompleted: false};
+    tasks.push(task);
 
     // 등록하면 입력창을 초기화
     taskInput.value = '';
