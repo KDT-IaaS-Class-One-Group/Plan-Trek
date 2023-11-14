baaf00b6b107876a7a818632294d2a4dc0363b20
Ho Nyeong <dev.honing@gmail.com>
2023-11-14 17:17:02 +0900

    Feat: 일정 목록 추가하기
    - addTask() 함수의 로직 작성

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,27 @@
 // main.js
 
+// 일정 목록 추가하는 함수 생성
+function addTask() {
+  const taskInput = document.getElementById("taskInput");
+  const taskText = taskInput.value.trim();
+  const ul = document.getElementById("taskList");
+
+  if ( taskText !== ''){
+    // span 생성
+    const taskSpan = document.createElement('span');
+    
+    // span에 taskText 내용 추가
+    taskSpan.textContent = taskText;
+
+    // li 생성
+    const taskLi = document.createElement('li');
+
+    // li에 span을 자식 요소로 추가
+    taskLi.appendChild(taskSpan);
+
+    // ul에 li를 자식 요소로 추가
+    ul.appendChild(taskLi);
+  } else {
+    console.error('일정 추가가 불가능합니다.')
+  }
+}
