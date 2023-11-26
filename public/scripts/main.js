fa4b5ae4265f42fdcf2bd9f1b2e4bf54eee7d174
Ho Nyeong <dev.honing@gmail.com>
2023-11-26 21:36:25 +0900

    Feat: Enter로 편리하게 일정 등록하는 기능 추가

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,100 +1,122 @@
 // main.js
 
-// 빈 배열 생성
-const tasks = [];
+// 
+document.addEventListener("DOMContentLoaded", function () {
+  // 빈 배열 생성
+  const tasks = [];
 
-// 일정 목록 추가하는 함수 생성
-function addTask() {
-  const taskInput = document.getElementById("taskInput");
-  const taskText = taskInput.value.trim();
-  const ul = document.getElementById("taskList");
+  // form 요소의 submit 이벤트를 차단
+  document.getElementById("taskForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
 
-  if ( taskText !== ''){
-    // li 생성
-    const taskLi = document.createElement('li');
+  // 일정 목록 추가하는 함수 생성
+  function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    const ul = document.getElementById("taskList");
 
-    // span 생성
-    const taskSpan = document.createElement('span');
-    
-    // 체크박스 생성
-    const checkbox = document.createElement("input");
-    checkbox.type = 'checkbox';
-    checkbox.checked = false;
+    if (taskText !== '') {
+      // li 생성
+      const taskLi = document.createElement('li');
 
-    // 체크박스의 변경 이벤트에 대한 핸들러를 등록
-    checkbox.onchange = function () {
-      completeTask(taskText);
+      // span 생성
+      const taskSpan = document.createElement('span');
+
+      // 체크박스 생성
+      const checkbox = document.createElement("input");
+      checkbox.type = 'checkbox';
+      checkbox.checked = false;
+
+      // 체크박스의 변경 이벤트에 대한 핸들러를 등록
+      checkbox.onchange = function () {
+        completeTask(tasks.length - 1);
+      }
+
+      // span에 taskText 내용 추가
+      taskSpan.textContent = taskText;
+
+      // li에 체크박스, span을 자식 요소로 추가
+      taskLi.appendChild(checkbox);
+      taskLi.appendChild(taskSpan);
+
+      // ul에 li를 자식 요소로 추가
+      ul.appendChild(taskLi);
+
+      // task 객체 생성 및 tasks 배열에 추가
+      const task = { text: taskText, isCompleted: false };
+      tasks.push(task);
+
+      // 등록하면 입력창을 초기화
+      taskInput.value = '';
+
+      // 변경된 상태로 일정 목록을 다시 표시
+      displayTasks();
+    } else {
+      console.error('일정 추가가 불가능합니다.')
     }
-
-    // span에 taskText 내용 추가
-    taskSpan.textContent = taskText;
-
-    // li에 체크박스, span을 자식 요소로 추가
-    taskLi.appendChild(checkbox);
-    taskLi.appendChild(taskSpan);
-
-    // ul에 li를 자식 요소로 추가
-    ul.appendChild(taskLi);
-
-    // task 객체 생성 및 tasks 배열에 추가
-    const task = { text: taskText, isCompleted: false};
-    tasks.push(task);
-
-    // 등록하면 입력창을 초기화
-    taskInput.value = '';
-  } else {
-    console.error('일정 추가가 불가능합니다.')
-  }
-}
-
-// 일정 완료 처리하는 함수
-function completeTask(taskIndex) {
-  // 해당 인덱스의 일정 상태를 토글
-  if (taskIndex >= 0 && taskIndex < tasks.length) {
-    tasks[taskIndex].isCompleted = !tasks[taskIndex].isCompleted;
   }
 
-  // 변경된 상태로 일정 목록을 다시 표시
-  displayTasks();
-}
-
-// 일정 목록 표시하는 함수
-function displayTasks() {
-  const taskList = document.getElementById('taskList');
-
-  // 기존 목록 모두 지우기
-  taskList.innerHTML = '';
-  
-  // 배열의 각 일정에 대해 li 요소와 span 요소 생성
-  tasks.forEach((task, index) => {
-    const taskItem = document.createElement('li');
-
-    // 체크박스 생성
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox'; // <input type="checkbox">
-    checkbox.checked = task.isCompleted;
-
-    // 체크박스 변경 이벤트에 핸들러 등록
-    checkbox.onchange = function () {
-      completeTask(index);
-    };
-
-    // span에 taskText 내용 추가
-    const taskSpan = document.createElement('span');
-    // task로 등록한 이름을 라벨링
-    taskSpan.innerHTML = `
-    <label>${task.text}</label>
-  `;
-
-
-    // 체크박스와 span을 li에 추가
-    taskItem.appendChild(checkbox);
-    taskItem.appendChild(taskSpan);
-    taskList.appendChild(taskItem);
-
-    // isCompleted 상태에 따라 취소선 적용
-    if (task.isCompleted) {
-      taskSpan.style.textDecoration = 'line-through';
+  // Enter 키로 호출되는 이벤트 핸들러 등록
+  document.getElementById("taskInput").addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // 기본 동작 막기
+      addTask(false); // 빈 값이면 추가되지 않도록 함
     }
   });
-}
+
+  document.getElementById("addTaskButton").addEventListener("click", function () {
+    addTask(false); // 빈 값이면 추가되지 않도록 함
+  });
+
+  // 일정 완료 처리하는 함수
+  function completeTask(taskIndex) {
+    // 해당 인덱스의 일정 상태를 토글
+    if (taskIndex >= 0 && taskIndex < tasks.length) {
+      tasks[taskIndex].isCompleted = !tasks[taskIndex].isCompleted;
+    }
+
+    // 변경된 상태로 일정 목록을 다시 표시
+    displayTasks();
+  }
+
+  // 일정 목록 표시하는 함수
+  function displayTasks() {
+    const taskList = document.getElementById('taskList');
+
+    // 기존 목록 모두 지우기
+    taskList.innerHTML = '';
+
+    // 배열의 각 일정에 대해 li 요소와 span 요소 생성
+    tasks.forEach((task, index) => {
+      const taskItem = document.createElement('li');
+
+      // 체크박스 생성
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox'; // <input type="checkbox">
+      checkbox.checked = task.isCompleted;
+
+      // 체크박스 변경 이벤트에 핸들러 등록
+      checkbox.onchange = function () {
+        completeTask(index);
+      };
+
+      // span에 taskText 내용 추가
+      const taskSpan = document.createElement('span');
+      // task로 등록한 이름을 라벨링
+      taskSpan.innerHTML = `<label>${task.text}</label>`;
+
+      // 체크박스와 span을 li에 추가
+      taskItem.appendChild(checkbox);
+      taskItem.appendChild(taskSpan);
+
+      // isCompleted 상태에 따라 취소선 적용
+      if (task.isCompleted) {
+        taskSpan.style.textDecoration = 'line-through';
+      }
+
+      // li를 ul에 추가
+      taskList.appendChild(taskItem);
+    });
+  }
+});
